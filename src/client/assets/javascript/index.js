0ee68cb77684501811318bc223016954b552748a
“Chaklader <CAREFE-EXT@DRIVEWEALTH.TECH>
2022-09-14 17:58:05 +0600

    Implemented the run race and run count down functionality

--- src/client/assets/javascript/index.js
+++ src/client/assets/javascript/index.js
@@ -102,24 +102,29 @@ async function handleCreateRace() {
 }
 
 function runRace(raceID) {
-    return new Promise(resolve => {
-        // TODO - use Javascript's built in setInterval method to get race info every 500ms
+    try {
+        return new Promise(resolve => {
 
-        /*
-            TODO - if the race info status property is "in-progress", update the leaderboard by calling:
+            const raceInterval = setInterval(async () => {
 
-            renderAt('#leaderBoard', raceProgress(res.positions))
-        */
+                const raceResponse = await getRace(raceID);
 
-        /*
-            TODO - if the race info status property is "finished", run the following:
+                if (raceResponse.status === 'in-progress') {
+                    renderAt('#leaderBoard', raceProgress(raceResponse.positions))
+                } else if (raceResponse.status === 'finished') {
+                    clearInterval(raceInterval);
+                    renderAt('#race', resultsView(raceResponse.positions));
+                    reslove(raceResponse);
 
-            clearInterval(raceInterval) // to stop the interval from repeating
-            renderAt('#race', resultsView(res.positions)) // to render the results view
-            reslove(res) // resolve the promise
-        */
-    })
-    // remember to add error handling for the Promise
+                }
+
+            }, 500)
+        })
+    }
+    catch (error){
+        console.log(`Error in runRace function::`, error.message);
+        console.error(error);
+    }
 }
 
 async function runCountdown() {
@@ -129,16 +134,19 @@ async function runCountdown() {
         let timer = 3
 
         return new Promise(resolve => {
-            // TODO - use Javascript's built in setInterval method to count down once per second
+            const countdownInterval = setInterval(()=>{
 
-            // run this DOM manipulation to decrement the countdown for the user
-            document.getElementById('big-numbers').innerHTML = --timer
-
-            // TODO - if the countdown is done, clear the interval, resolve the promise, and return
+                document.getElementById('big-numbers').innerHTML = --timer;
+                if (timer <= 0){
+                    clearInterval(countdownInterval);
+                    resolve('done');
+                }
+            }, 1000);
 
         })
     } catch (error) {
-        console.log(error);
+        console.log(`Error in runCountdown function::`, error.message);
+        console.error(error);
     }
 }
 
